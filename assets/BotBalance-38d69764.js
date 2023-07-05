ad6a8610f7f19159c2bb6b51a1ea2ccc0acbbe98
Tako <me@example.com>
2023-07-05 20:53:23 +0000

    bump

---
+++ assets/BotBalance-38d69764.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-e07c030f.js";import"./index-2cf5a4f6.js";import"./install-c378bec5.js";import"./createSeriesDataSimply-c54d4ad0.js";export{o as default};
+//# sourceMappingURL=BotBalance-38d69764.js.map
--- assets/createSeriesDataSimply.d3428088.js
+++
@@ -1,2 +0,0 @@
-import{G as t,K as c,br as m,b3 as o}from"./install.1a6288f8.js";function S(a,e,n){e=t(e)&&{coordDimensions:e}||c({encodeDefine:a.getEncode()},e);var r=a.getSource(),s=m(r,e).dimensions,i=new o(s,a);return i.initData(r,n),i}export{S as c};
-//# sourceMappingURL=createSeriesDataSimply.d3428088.js.map
